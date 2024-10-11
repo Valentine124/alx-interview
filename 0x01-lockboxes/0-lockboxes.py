7b42ab517e06b7c752d3cf8bd57b8fe4604279e7
Valentine124 <kaluvalentine60@gmail.com>
2024-10-11 03:53:29 +0100

    Fixed pycodestylr error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,24 +6,24 @@ algorithm question
 
 
 def canUnlockAll(boxes):
-  """ Returns True if the boxes can be unlocked """
+    """ Returns True if the boxes can be unlocked """
 
-  keys = {0}
+    keys = {0}
 
-  if not (type(boxes) is list):
-    return False
-  
-  for box in boxes:
-    if not (type(box) is list):
-      return False
-    for key in box:
-      idx = boxes.index(box)
+    if not (type(boxes) is list):
+        return False
 
-      if not (key == idx):
-        keys.add(key)
-        
-  for num in range(0, len(boxes)):
-    if not (num in keys):
-      return False
-  
-  return True
+    for box in boxes:
+        if not (type(box) is list):
+            return False
+        for key in box:
+            idx = boxes.index(box)
+
+        if not (key == idx):
+            keys.add(key)
+
+    for num in range(0, len(boxes)):
+        if not (num in keys):
+            return False
+
+    return True

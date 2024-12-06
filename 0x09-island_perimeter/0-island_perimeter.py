407b1760f601a1f108d69b0c7c15966b8a91f7fa
Valentine124 <kaluvalentine60@gmail.com>
2024-12-06 01:07:49 +0100

    Solved the Island Perimeter Problem

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,39 +3,39 @@
 
 
 def island_perimeter(grid):
-  """Implementation of the Solution"""
+    """Implementation of the Solution"""
 
-  if grid == None:
-    return None
-  
-  perimeter = 0
-  grid_len = len(grid)
+    if grid is None:
+        return None
 
-  for i in range(0, grid_len):
-    row_len = len(grid[i])
-    for j in range(0, row_len):
-      if grid[i][j] == 1:
-        if i != 0:
-          if grid[i - 1][j] != 1:
-            perimeter += 1
-        else:
-          perimeter += 1
-        
-        if j != row_len - 1:
-          if grid[i][j + 1] != 1:
-            perimeter += 1
-        else:
-          perimeter += 1
-        
-        if i != grid_len - 1:
-          if grid[i + 1][j] != 1:
-            perimeter += 1
-        else:
-          perimeter += 1
-        
-        if j != 0:
-          if grid[i][j - 1] != 1:
-            perimeter += 1
-        else:
-          perimeter += 1
-  return perimeter
+    perimeter = 0
+    grid_len = len(grid)
+
+    for i in range(0, grid_len):
+        row_len = len(grid[i])
+        for j in range(0, row_len):
+            if grid[i][j] == 1:
+                if i != 0:
+                    if grid[i - 1][j] != 1:
+                        perimeter += 1
+                else:
+                    perimeter += 1
+
+            if j != row_len - 1:
+                if grid[i][j + 1] != 1:
+                    perimeter += 1
+            else:
+                perimeter += 1
+
+            if i != grid_len - 1:
+                if grid[i + 1][j] != 1:
+                    perimeter += 1
+            else:
+                perimeter += 1
+
+            if j != 0:
+                if grid[i][j - 1] != 1:
+                    perimeter += 1
+            else:
+                perimeter += 1
+    return perimeter
